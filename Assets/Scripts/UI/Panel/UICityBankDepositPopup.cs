dd9c4a877635a4e48c34c4ea8831f136a90ac65c
ParkGwanHong <ganonga40@gmail.com>
2023-12-05 01:49:08 +0900

    은행 예금 메뉴 추가

--- Assets/Scripts/UI/Panel/UICityBankDepositPopup.cs
+++ Assets/Scripts/UI/Panel/UICityBankDepositPopup.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UICityBankDepositPopup : UIBase
 {
+    [Header("첫 메뉴")]
+    public GameObject kFrontMenuGo;
+
+    [Header("입금 메뉴")]
+    public GameObject kDepositMenuGo;
+
+    [Header("출금 메뉴")]
+    public GameObject kWithdrawMenuGo;
+    public TMP_InputField kMyGoldTextInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +25,39 @@ public class UICityBankDepositPopup : UIBase
     void Update()
     {
         
+    }
+
+    protected override void onEnable()
+    {
+        kFrontMenuGo.SetActive(true);
+        kDepositMenuGo.SetActive(false);
+        kWithdrawMenuGo.SetActive(false);
+    }
+
+    public void OnDepositMenuButtonClick()
+    {
+        kFrontMenuGo.SetActive(false);
+        kDepositMenuGo.SetActive(true);
+    }
+
+    public void OnDepositMenuCancelButtonClick()
+    {
+        kFrontMenuGo.SetActive(true);
+        kDepositMenuGo.SetActive(false);
+    }
+
+    public void OnWithdrawMenuButtonClick()
+    {
+        kFrontMenuGo.SetActive(false);
+        kWithdrawMenuGo.SetActive(true);
+
+        kMyGoldTextInput.text = Mng.data.myInfo.gold.ToColumnString() + " G";
+    }
+
+    public void OnWithdrawMenuCancelButtonClick()
+    {
+        kFrontMenuGo.SetActive(true);
+        kWithdrawMenuGo.SetActive(false);        
     }
 
     public void OnCloseButton()
